b1770cbdca34e648a5366002521815b1f5e87798
ngtrgiabao <ngtrgiabao251002@gmail.com>
2024-03-27 14:16:47 +0700

    feat: list excercises

--- App.js
+++ App.js
@@ -1,45 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, View} from 'react-native';
 import exercises from "./assets/data/exercises.json"
+import ExcerciseList from "./src/components/ExcerciseList";
 
 export default function App() {
-  console.log(exercises)
-  const excercise = exercises[0]
-
-  return (
-    <View style={styles.container}>
-      <View style={styles.excerciseContainer}>
-        <Text style={styles.exerciseName}>
-          {excercise.name}
-        </Text>
-        <Text style={styles.exerciseSubtitle}>
-          {excercise.muscle.toUpperCase()} | Equipment: {excercise.equipment.toUpperCase()}
-        </Text>
-      </View>
-
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <ExcerciseList exercises={exercises}/>
+            <StatusBar style="auto"/>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#ccc',
-    justifyContent: 'center',
-    padding: 10
-  },
-  excerciseContainer: {
-    backgroundColor: "#fff",
-    padding: 10,
-    borderRadius: 10,
-    gap: 5,
-  },
-  exerciseName: {
-    fontSize: 20,
-    fontWeight: '500',
-  },
-  exerciseSubtitle: {
-    color: 'dimgray'
-  }
+    container: {
+        flex: 1,
+        backgroundColor: '#ccc',
+        justifyContent: 'center',
+        padding: 10
+    },
 });
---
+++ src/components/ExcerciseList.jsx
@@ -0,0 +1,45 @@
+import {StyleSheet, View, FlatList, Text} from 'react-native';
+
+const ExcerciseItem = ({item}) => (
+    <View style={styles.excerciseContainer}>
+        <Text style={styles.exerciseName}>
+            {item.name}
+        </Text>
+        <Text style={styles.exerciseSubtitle}>
+            {item.muscle.toUpperCase()} | Equipment: {item.equipment.toUpperCase()}
+        </Text>
+    </View>
+)
+
+
+const ExcerciseList = ({exercises}) => {
+    return (
+        <FlatList
+            data={exercises}
+            keyExtractor={(item, idx) => item.name + idx}
+            contentContainerStyle={{gap: 5}}
+            renderItem={
+                ({item}) =>
+                    <ExcerciseItem item={item}/>
+            }
+        />
+    )
+}
+
+export default ExcerciseList;
+
+const styles = StyleSheet.create({
+    excerciseContainer: {
+        backgroundColor: "#fff",
+        padding: 10,
+        borderRadius: 10,
+        gap: 5,
+    },
+    exerciseName: {
+        fontSize: 20,
+        fontWeight: '500',
+    },
+    exerciseSubtitle: {
+        color: 'dimgray'
+    }
+});
